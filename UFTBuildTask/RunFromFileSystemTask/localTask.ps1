84ac16d52512579a7172172a3b93ec118d78cb9c
Dorin Bogdan <dorin.bogdan@microfocus.com>
2023-10-20 18:58:08 +0300

    MIT license and copyright notice updates

--- UFTBuildTask/RunFromFileSystemTask/localTask.ps1
+++ UFTBuildTask/RunFromFileSystemTask/localTask.ps1
@@ -1,8 +1,4 @@
 # 
-# Certain versions of software accessible here may contain branding from Hewlett-Packard Company (now HP Inc.) and Hewlett Packard Enterprise Company.
-# This software was acquired by Micro Focus on September 1, 2017, and is now offered by OpenText.
-# Any reference to the HP and Hewlett Packard Enterprise/HPE marks is historical in nature, and the HP and Hewlett Packard Enterprise/HPE marks are the property of their respective owners.
-# __________________________________________________________________
 # MIT License
 # 
 # Copyright 2016-2023 Open Text
@@ -15,13 +11,6 @@
 # omissions contained herein. The information contained herein is subject
 # to change without notice.
 # 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ___________________________________________________________________
-#
 
 using namespace PSModule.UftMobile.SDK.UI
 using namespace PSModule.UftMobile.SDK.Entity
