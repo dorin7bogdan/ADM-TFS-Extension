f6a8948e7ee5649391f5d08695f5707b39713c2b
Dorin Bogdan <48766961+dorin7bogdan@users.noreply.github.com>
2024-03-27 15:29:27 +0200

    add missed file (#88)
    Co-authored-by: Dorin Bogdan <dorin.bogdan@microfocus.com>

---
+++ PSModule/Common/EnvVarsConfig.cs
@@ -0,0 +1,16 @@
+﻿
+using PSModule.UftMobile.SDK.UI;
+
+namespace PSModule.Common
+{
+    public class EnvVarsConfig(string storageAccount, string container, string leaveUftOpenIfVisible = "") : IConfig
+    {
+        private readonly string _storageAccount = storageAccount;
+        private readonly string _container = container;
+        private readonly string _leaveUftOpenIfVisible = leaveUftOpenIfVisible;
+
+        public string StorageAccount => _storageAccount;
+        public string Container => _container;
+        public string LeaveUftOpenIfVisible => _leaveUftOpenIfVisible;
+    }
+}
